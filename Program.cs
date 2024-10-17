5678755168ab20f90dd6c7e1058d199b250b9c49
msiwy12345 <mateuszsiwy00@gmail.com>
2024-10-17 15:07:08 +0200

    Added basic HTML view

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+/*builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAllOrigins", builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+    });*/
 var app = builder.Build();
 app.UseHttpsRedirection();
 
-
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapControllers();
 app.Run();
---
+++ wwwroot/js/site.js
@@ -0,0 +1,46 @@
+﻿
+
+document.getElementById('apartmentButton').addEventListener('click', getApartments);
+
+function getApartments() {
+    console.log('button clicked');
+    fetch('api/apartment')
+        .then((res) => res.json())
+        .then((data) => {
+            let content = `
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Price</th>
+                            <th>Number of Rooms</th>
+                            <th>Owner ID</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+            `;
+            data.forEach(apartment => {
+                content += `
+                    <tr>
+                        <td>${apartment.id}</td>
+                        <td>${apartment.name}</td>
+                        <td>${apartment.address}</td>
+                        <td>${apartment.price}</td>
+                        <td>${apartment.numberOfRooms}</td>
+                        <td>${apartment.ownerId}</td> 
+                    </tr>
+                `;
+                    
+                    
+            });
+            content += `
+                    </tbody>
+                </table>
+            `;
+
+
+            document.getElementById('tableContent').innerHTML = content;
+        })
+}
